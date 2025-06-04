93222211770431e090e3e6a135d2e64e8cb1b087
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2025-06-03 18:07:40 -0600

    Reparacion de forma de pago con saldo a favor (otros) y cheque o transferencia 1.1

--- js/billingFunctions.js
+++ js/billingFunctions.js
@@ -146,6 +146,8 @@ var global_sale = null, global_costumer = null;
                 $( '#payment_type' ).html( '<option value="1">EFECTIVO</option>' );
             }else if( payments_types.length == 1 && payments_types[0] == 2 ){
                 $( '#payment_type' ).html( '<option value="17">OTROS</option>' );
+            }else if( payments_types.length == 1 && payments_types[0] == 8 ){
+                                    $( '#payment_type' ).html( '<option value="9">TRANSFERENCIA</option>' );
             }else if( payments_types.length == 1 ){//&& payments_types[0] == 1 
             //recorre tipos de pagos
                 payments_types = new Array();
@@ -157,14 +159,8 @@ var global_sale = null, global_costumer = null;
                                     $( '#payment_type' ).html( '<option value="11">TARJETA DE CRÉDITO</option>' );
                                 }else if(  $( this ).val() == 14 ){
                                     $( '#payment_type' ).html( '<option value="14">TARJETA DE DÉDITO</option>' );
-                                }else if(  $( this ).val() == 8 ){
-                                    $( '#payment_type' ).html( '<option value="9">TRANSFERENCIA</option>' );
                                 }
                             });
-                            //alert( $(this).attr("value") );
-                            //if( ! payments_types.includes( $(this).attr("value") ) ){
-                              //  payments_types.push( $(this).attr("value") );
-                           // }
                         }
                     });
                 });
