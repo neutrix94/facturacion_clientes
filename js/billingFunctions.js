f1ad8aefe84176a85636ce2714aa030068ff53a4
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2025-06-04 12:08:19 -0600

    Se agrega la url de descarga de archivos para la facturacion del cliente 1.6

--- js/billingFunctions.js
+++ js/billingFunctions.js
@@ -126,8 +126,10 @@ var global_sale = null, global_costumer = null;
             $( '#download_container' ).removeClass( 'hidden' );//hace visible boton para descargar archivos
             $( '#send_email_btn' ).attr( "sale_folio", `${global_sale.sale.folio}` );
             $( '#email_container' ).removeClass( 'hidden' );//hace visible boton para enviar correo
-            $( '#bill_container' ).addClass( "hidden" );//oculta boton de facturacion
-            //$( '#bill_container' ).css( "display", "none" );//oculta boton de facturacion
+            //$( '#bill_container' ).addClass( "hidden" );//oculta boton de facturacion
+            $( '#bill_container' ).css( "display", "none" );//oculta boton de facturacion
+            $( '#payment_type_container' ).css( "display", "none" );//oculta boton de facturacion
+            
         }
     }
 
