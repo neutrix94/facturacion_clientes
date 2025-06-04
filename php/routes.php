e19b11bbcddf213976d40a0119b0f914efa1eaa9
neutrix94 <109834526+neutrix94@users.noreply.github.com>
2025-06-04 12:43:20 -0600

    actualizacion de metodo de pago en genreacion de factura RS

--- php/routes.php
+++ php/routes.php
@@ -30,7 +30,8 @@
                 $sale_folio = ( isset( $_POST['sale_folio'] ) ? $_POST['sale_folio'] : $_GET['sale_folio'] );
                 $sale_costumer = ( isset( $_POST['sale_costumer'] ) ? $_POST['sale_costumer'] : $_GET['sale_costumer'] );
                 $cfdi = ( isset( $_POST['cfdi'] ) ? $_POST['cfdi'] : $_GET['cfdi'] );
-                $post_data = json_encode( array( "sale_folio"=>$sale_folio, "sale_costumer"=>$sale_costumer, "cfdi_use"=>$cfdi ) );
+                $payment_type = ( isset( $_POST['payment_type'] ) ? $_POST['payment_type'] : $_GET['payment_type'] );
+                $post_data = json_encode( array( "sale_folio"=>$sale_folio, "sale_costumer"=>$sale_costumer, "cfdi_use"=>$cfdi, "payment_type"=>$payment_type ) );
                 //die( "here : " . $post_data );
                 $url = $Routes->getPath( 'billing_api' );
                 echo $Routes->sendPetition( "{$url}/rest/solicitud_factura", $post_data );//inserta_venta_sistema_facturacion
